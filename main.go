42d3d049ab81ca3d8b0f9c36e2f29a0499536eec
Andy Walker <walkeraj@gmail.com>
2015-05-05 18:43:46 -0400

    check whether devices have been added or removed every second

--- main.go
+++ main.go
@@ -2,18 +2,60 @@ package main
 
 import (
 	"log"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
-	devices, err := enumerateDevices()
+	initialDevices, err := enumerateDevices()
 	if err != nil {
 		log.Fatal(err)
 	}
-	if len(devices) > 0 {
-		for _, d := range devices {
-			log.Printf("Device: %s, ID: %s", d.Name, d.ID)
+
+	deviceMap := map[string]int64{}
+
+	if len(initialDevices) > 0 {
+		now := time.Now().UnixNano()
+		for _, d := range initialDevices {
+			deviceMap[d.ID] = now
 		}
 	} else {
 		log.Fatal("No devices found")
+	}
+
+	sigint := make(chan os.Signal, 1)
+	signal.Notify(sigint, os.Interrupt)
+
+	check := time.NewTicker(1 * time.Second)
+
+	for {
+		select {
+		case <-check.C:
+			now := time.Now().UnixNano()
+			devices, err := enumerateDevices()
+			if err != nil {
+				log.Fatal(err)
+			}
+			// Look to see if there are any new devicies
+			for _, d := range devices {
+				if _, ok := deviceMap[d.ID]; !ok {
+					log.Printf("New device: %s\n", d.Name)
+					os.Exit(1) // run commands
+				}
+				deviceMap[d.ID] = now
+			}
+
+			// Look to see if any devices have been removed
+			for id, t := range deviceMap {
+				if t != now {
+					log.Printf("Device with id %s has been removed\n", id)
+					os.Exit(1) //run commands
+				}
+			}
+		case <-sigint:
+			log.Println("SIGINT received")
+			os.Exit(0)
+		}
 	}
 }
