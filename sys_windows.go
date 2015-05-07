fd1d745118ce31da17bbcbcc9e56d1f89584656f
Andy Walker <walkeraj@gmail.com>
2015-05-06 21:29:06 -0400

    add force flag to windows shutdown
    Hopefully this addresses issue #7. More testing necessary before
    closing.

--- sys_windows.go
+++ sys_windows.go
@@ -39,7 +39,7 @@ func shutdownNow() error {
 	if err := checkExe("shutdown"); err != nil {
 		return err
 	}
-	err := exec.Command("shutdown", "/p").Run()
+	err := exec.Command("shutdown", "/f", "/p").Run()
 
 	//Not that this matters
 	return err
