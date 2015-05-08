db6703c5e1cef6c94c9b7090b03a9c189f84f682
Andy Walker <walkeraj@gmail.com>
2015-05-08 14:43:23 -0400

    credit where it's due
    Forgot to credit pwnsdx for the idea he proposed for killing loginwindow
    and Finder on OSX. A very smart idea.
    
    ref:
    https://github.com/hephaest0s/usbkill/pull/22#commitcomment-11066497

--- sys_darwin.go
+++ sys_darwin.go
@@ -48,6 +48,8 @@ func shutdownNow() error {
 	if err := checkExe("shutdown"); err != nil {
 		return err
 	}
+	// kill loginwindow and finder to limit access as much as possible quickly
+	// thanks, pwnsdx!
 	if err := exec.Command("killall", "loginwindow", "Finder").Run(); err != nil {
 		return err
 	}
