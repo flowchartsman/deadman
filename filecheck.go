2728b83ada32d79c8c273d11cd7ff670687c3715
CowboySteeve <chris.straight@stackvikings.com>
2019-11-23 18:18:46 -0800

    Adding executible verification in checkExe function.

--- filecheck.go
+++ filecheck.go
@@ -4,8 +4,51 @@ import (
 	"os/exec"
 )
 
-func checkExe(path string) error {
+func checkExe(path string) (bool, error) {
 	_, err := exec.LookPath(path)
-	return err
-	//TODO: check for executability
+	if err != nil {
+		return err
+	}
+	
+	cmd := "which"
+	args := []string{path}
+	execCmd := exec.Command(cmd, args...)
+	
+	var out bytes.Buffer
+    var stderr bytes.Buffer
+
+    execCmd.Stdout = &out
+    execCmd.Stderr = &stderr
+    
+    err := execCmd.Run()
+    if err != nil {
+        fmt.Println("out: " + out.String())
+        fmt.Println("stderr: " + stderr.String())
+        fmt.Println(err)
+    }
+
+    info, err := os.Stat("/usr/bin/lsusb")
+    if err != nil {
+		return false, err
+	}
+
+	m := info.Mode()
+	
+	/*
+	// Bitshift to check if any user can execute given binary
+	fmt.Println(m&(2>>1))
+
+	// Bitshift to check if group can execute given binary
+	fmt.Println(m&(1<<3))
+
+	// Bitshift to check if owner can execute given binary
+	fmt.Println(m&(1<<6))
+	*/
+
+	// Bitshift to check if any user can execute given binary
+	if m&(2>>1) != 0 {
+    	return true, nil
+	} else {
+    	return false, nil
+	}
 }
