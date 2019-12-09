c22c35c9daaeb60a475dd4dd905e05e434418e46
CowboySteeve <chris.straight@stackvikings.com>
2019-12-09 14:04:13 -0800

    Adding check if file is executable.

--- filecheck.go
+++ filecheck.go
@@ -1,54 +1,57 @@
 package main
 
 import (
+	"bytes"
+	"log"
+	"os"
 	"os/exec"
 )
 
-func checkExe(path string) (bool, error) {
+func checkExe(path string) error {
 	_, err := exec.LookPath(path)
 	if err != nil {
 		return err
 	}
-	
-	cmd := "which"
+
+	cmd := "which"	
 	args := []string{path}
 	execCmd := exec.Command(cmd, args...)
-	
+
 	var out bytes.Buffer
-    var stderr bytes.Buffer
+	var stderr bytes.Buffer
 
-    execCmd.Stdout = &out
-    execCmd.Stderr = &stderr
-    
-    err := execCmd.Run()
-    if err != nil {
-        fmt.Println("out: " + out.String())
-        fmt.Println("stderr: " + stderr.String())
-        fmt.Println(err)
-    }
+	execCmd.Stdout = &out
+	execCmd.Stderr = &stderr
 
-    info, err := os.Stat("/usr/bin/lsusb")
-    if err != nil {
-		return false, err
+	err = execCmd.Run()
+	if err != nil {
+		log.Println("out: " + out.String())
+		log.Println("stderr: " + stderr.String())
+		log.Println(err)
+	}
+
+	info, err := os.Stat("/usr/bin/lsusb")
+	if err != nil {
+		return err
 	}
 
 	m := info.Mode()
-	
+
 	/*
-	// Bitshift to check if any user can execute given binary
-	fmt.Println(m&(2>>1))
+		// Bitshift to check if any user can execute given binary
+		log.Println(m&(2>>1))
 
-	// Bitshift to check if group can execute given binary
-	fmt.Println(m&(1<<3))
+		// Bitshift to check if group can execute given binary
+		log.Println(m&(1<<3))
 
-	// Bitshift to check if owner can execute given binary
-	fmt.Println(m&(1<<6))
+		// Bitshift to check if owner can execute given binary
+		log.Println(m&(1<<6))
 	*/
 
 	// Bitshift to check if any user can execute given binary
 	if m&(2>>1) != 0 {
-    	return true, nil
+		return nil
 	} else {
-    	return false, nil
+		return nil
 	}
 }
