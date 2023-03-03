ef040b2a6bc2356740833a375bfac60bf03e491c
Chris <straight.chris@gmail.com>
2023-03-03 15:04:34 -0800

    Refactored enumerateDevices() to reduce its Cognitive Complexity.

--- usb_enum_freebsd.go
+++ usb_enum_freebsd.go
@@ -9,30 +9,31 @@ func enumerateDevices() ([]device, error) {
 	if err := checkExe("usbconfig"); err != nil {
 		return nil, err
 	}
+
 	out, err := exec.Command("usbconfig", "-d", "ugen*").Output()
 	if err != nil {
 		return nil, err
 	}
 
 	var deviceList []device
-	devices := strings.Split(string(out), "\n\n")
+	var currentDevice device
 
-	for _, d := range devices {
-		if strings.HasPrefix(d, "ugen") {
-			lines := strings.Split(d, "\n")
-			for _, line := range lines {
-				if strings.HasPrefix(line, "  name:") {
-					name := strings.TrimSpace(strings.TrimPrefix(line, "  name:"))
-					deviceList = append(deviceList, device{name, ""})
-				} else if strings.HasPrefix(line, "  devname:") {
-					id := strings.TrimSpace(strings.TrimPrefix(line, "  devname:"))
-					if len(deviceList) > 0 {
-						deviceList[len(deviceList)-1].ID = id
-					}
-				}
+	for _, line := range strings.Split(string(out), "\n") {
+		line = strings.TrimSpace(line)
+		if line == "" {
+			if currentDevice.Name != "" {
+				deviceList = append(deviceList, currentDevice)
 			}
+			currentDevice = device{}
+			continue
+		}
+		if strings.HasPrefix(line, "name:") {
+			currentDevice.Name = strings.TrimSpace(strings.TrimPrefix(line, "name:"))
+		} else if strings.HasPrefix(line, "devname:") {
+			currentDevice.ID = strings.TrimSpace(strings.TrimPrefix(line, "devname:"))
 		}
 	}
+
 	if len(deviceList) == 0 {
 		return nil, nil
 	}
