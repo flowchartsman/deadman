b4e1408f1bf00e8c9dd8468e9eb3e8555ec21cc5
Chris <straight.chris@gmail.com>
2023-03-03 16:25:24 -0800

    Call the reboot system call directly using syscall package for Linux only. The function attempts to call the reboot system call with the LINUX_REBOOT_CMD_POWER_OFF command first and, if that fails, with the LINUX_REBOOT_CMD_HALT command. If both calls fail, the function returns an error.

--- shutdown_linux.go
+++ shutdown_linux.go
@@ -1,15 +1,22 @@
 package main
 
 import (
-	"os/exec"
+	"fmt"
+	"syscall"
 )
 
 func shutdownNow() error {
-	if err := checkExe("shutdown"); err != nil {
-		return err
-	}
-	err := exec.Command("shutdown", "-h", "now").Run()
 
-	//Not that this matters
-	return err
+	// First, try to call the reboot system call with the appropriate arguments
+	if err := syscall.Reboot(syscall.LINUX_REBOOT_CMD_POWER_OFF); err == nil {
+		return nil
+	}
+
+	// If that fails, try to call the halt system call with the appropriate arguments
+	if err := syscall.Reboot(syscall.LINUX_REBOOT_CMD_HALT); err == nil {
+		return nil
+	}
+
+	// If both system calls fail, return an error
+	return fmt.Errorf("failed to initiate system shutdown")
 }
